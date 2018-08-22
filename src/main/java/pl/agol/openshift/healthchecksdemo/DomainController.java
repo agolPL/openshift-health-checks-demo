d8c1553e1d225865c84859e6f35b9feda574bdd0
agolPL <andipansa@gmail.com>
2018-08-22 20:02:40 +0100

    add new endpoints

--- src/main/java/pl/agol/openshift/healthchecksdemo/DomainController.java
+++ src/main/java/pl/agol/openshift/healthchecksdemo/DomainController.java
@@ -1,21 +1,31 @@
 package pl.agol.openshift.healthchecksdemo;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/api/hello")
+@RequestMapping("api")
 class DomainController {
 
-    @GetMapping
-    String getHello(@RequestParam("requestDelay") int requestDelay) throws InterruptedException {
+    private int requestDelay = 0;
+
+    @PostMapping("hi")
+    void setDelay(@RequestBody int requestDelay) {
+        this.requestDelay = requestDelay;
+        log.info("New value of delay: {} ms", requestDelay);
+    }
+
+    @GetMapping("hello")
+    String getHello() throws InterruptedException {
         log.info("Get request and going to sleep: {} ms", requestDelay);
         Thread.sleep(requestDelay);
         log.info("Now I'm ready to handle the request");
         return "hello";
+    }
+
+    @GetMapping("hi")
+    String getHi() {
+        return "hi";
     }
 }
